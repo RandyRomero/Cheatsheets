523deb0cde98dce7d279c5f199658365d313ec94
RandyRomero <a.ya.miheev@gmail.com>
2019-11-02 21:12:26 +0300

    make example of removing rows with nan in columns; add settings for pandas to display a more rows and columns

--- pandas/pandas_cheatsheet.py
+++ pandas/pandas_cheatsheet.py
@@ -1,13 +1,45 @@
 import pandas as pd
 
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.width', 1000)
+
 
 def print_specific_columns(df):
     print(df[['date', 'name']])
 
 
+def remove_rows_with_nan_values(df: pd.DataFrame) -> None:
+    """
+    remove rows if there is a nan value in a specified column
+
+    :param df: some typical dataframe
+    :return: None
+    """
+    # preparing (make some columns contain nan values)
+    df_with_nan = df.copy(deep=True)
+    df_with_nan[df_with_nan[['all cores', 'one core',
+                             'total score']] < 1000] = pd.np.nan
+
+    # Remove_rows_with_missing_values
+    # P.S. axis=0 means to delete rows (not columns)
+    # P.S2. subset lets you specify which columns to check for nan
+    # (instead of all columns)
+    columns_to_check = ['all cores', 'one core', 'total score']
+    na_free = df_with_nan.dropna(axis=0, subset=columns_to_check)
+
+    # save rows which were dropped to show them
+    only_na = df_with_nan[~df_with_nan.index.isin(na_free.index)]
+    print(f'{only_na.shape[0]} rows were dropped because they contain empty '
+          f'values in columns: {", ".join(columns_to_check)}. '
+          f'There these rows are:\n')
+    print(only_na)
+
+
 def main():
     df = pd.read_excel('smartphones.xlsx')
-    print_specific_columns(df)
+    # print_specific_columns(df)
+    remove_rows_with_nan_values(df)
 
 
 if __name__ == '__main__':
