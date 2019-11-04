c116688aba30b437e113b7c4788d774af57de40e
RandyRomero <a.ya.miheev@gmail.com>
2019-11-04 23:35:01 +0300

    add a new interesting task to solve, based on python itertools.groupby()

---
+++ python/itertools/binnigns question.py
@@ -0,0 +1,44 @@
+# based on
+# https://stackoverflow.com/questions/11270991/divide-a-list-into-multiple-
+# lists-based-on-a-bin-size/45991797#45991797
+
+"""
+I have a list containing more than 100,000 values in it.
+
+I need to divide the list into multiple smaller lists based on a specific bin
+width say 0.1. Can anyone help me how to write a python program to do this?
+"""
+
+from itertools import groupby
+
+
+class Binnings:
+    def __init__(self, items):
+        self.items = items
+
+    @staticmethod
+    def _key(x):
+        return x[0]
+
+    def group_by_first_digit_after_dot(self):
+        grouped_binnings = {}
+        # your code here
+
+        return grouped_binnings
+
+
+binnings = Binnings(items=[-0.234,
+                           -0.04325,
+                           -0.43134,
+                           -0.315,
+                           -0.6322,
+                           -0.245,
+                           -0.5325,
+                           -0.6341,
+                           -0.5214,
+                           -0.531,
+                           -0.124,
+                           -0.0252,
+                           ])
+
+print(binnings.group_by_first_digit_after_dot())
---
+++ python/itertools/binnings answer.py
@@ -0,0 +1,41 @@
+# https://stackoverflow.com/questions/11270991/divide-a-list-into-multiple-
+# lists-based-on-a-bin-size/45991797#45991797
+
+from itertools import groupby
+
+
+class Binnings:
+    def __init__(self, items):
+        self.items = items
+
+    @staticmethod
+    def _key(x):
+        return x[0]
+
+    def group_by_first_digit_after_dot(self):
+        grouped_binnings = {}
+        tmp = tuple(tuple([round(x, 1), x]) for x in self.items)
+
+        for key, group in groupby(sorted(tmp, key=self._key), key=self._key):
+            grouped_binnings[key] = []
+            for item in group:
+                grouped_binnings[key].append(item[1])
+
+        return grouped_binnings
+
+
+binnings = Binnings(items=[-0.234,
+                           -0.04325,
+                           -0.43134,
+                           -0.315,
+                           -0.6322,
+                           -0.245,
+                           -0.5325,
+                           -0.6341,
+                           -0.5214,
+                           -0.531,
+                           -0.124,
+                           -0.0252,
+                           ])
+
+print(binnings.group_by_first_digit_after_dot())
