b6dcc794caab7479d10e11e93b16cbf2a4de0734
RandyRomero <a.ya.miheev@gmail.com>
2019-11-04 17:46:19 +0300

    add folder for python cheatsheets and for python itertools cheatsheets; add question and answer about itertools groupby

---
+++ python/itertools/itertools_groupby_answer.py
@@ -0,0 +1,23 @@
+from itertools import groupby
+
+things = [
+    ("plant", "cucumber"),
+    ("animal", "bear"),
+    ("vehicle", "school bus"),
+    ("animal", "duck"),
+    ("vehicle", "speed boat"),
+    ("plant", "cactus"),
+          ]
+
+
+def key(x): return x[0]
+
+
+# groupby only works on sorted iterables
+for key, group in groupby(sorted(things, key=key), key=key):
+    print(list(group))
+
+# output
+# [('animal', 'bear'), ('animal', 'duck')]
+# [('plant', 'cucumber'), ('plant', 'cactus')]
+# [('vehicle', 'school bus'), ('vehicle', 'speed boat')]
---
+++ python/itertools/itertools_groupby_question.py
@@ -0,0 +1,17 @@
+from itertools import groupby
+
+things = [
+    ("plant", "cucumber"),
+    ("animal", "bear"),
+    ("vehicle", "school bus"),
+    ("animal", "duck"),
+    ("vehicle", "speed boat"),
+    ("plant", "cactus"),
+          ]
+
+# your code here...
+
+# output
+# [('animal', 'bear'), ('animal', 'duck')]
+# [('plant', 'cucumber'), ('plant', 'cactus')]
+# [('vehicle', 'school bus'), ('vehicle', 'speed boat')]
